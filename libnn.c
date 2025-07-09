43e6990b83b6e9787dabfde832912782bbb4cad2
Jacob Niemeir <nniemeir@protonmail.com>
2025-07-09 10:30:41 -0500

    Error handling and modularity improvements

--- libnn.c
+++ libnn.c
@@ -1,11 +1,17 @@
 #include "libnn.h"
 
-const char *malloc_err = "Failed to allocate memory.";
-const char *prepend_err = "Failed to prepend program data path to string.";
-
 int file_exists(const char *filename) {
   struct stat buffer;
-  return stat(filename, &buffer) == 0 ? 1 : 0;
+  int result = stat(filename, &buffer);
+  if (result == -1) {
+    fprintf(stderr, "Failed to get status of file %s: %s", filename,
+            strerror(errno));
+    return 0;
+  }
+  if (result == 0) {
+    return 1;
+  }
+  return 0;
 }
 
 char *get_file_extension(const char *file_path) {
@@ -17,172 +23,224 @@ char *get_file_extension(const char *file_path) {
   return file_extension;
 }
 
-unsigned char *read_file(const char *program_name, const char *file_path,
-                         size_t *file_size) {
+unsigned char *read_file(const char *file_path, size_t *file_size) {
   FILE *file = fopen(file_path, "rb");
   if (!file) {
-    log_event(program_name, ERROR,
-              "read_file failed to open the requested file.", log_to_file);
+    fprintf(stderr, "Failed to open file %s:%s\n", file_path, strerror(errno));
     return NULL;
   }
 
   if (fseek(file, 0, SEEK_END) == -1) {
-    log_event(program_name, ERROR, "fseek failed.", log_to_file);
+    fprintf(stderr, "Failed to set file position indicator: %s\n",
+            strerror(errno));
     fclose(file);
     return NULL;
   }
 
-  long size = ftell(file);
+  int size = ftell(file);
   if (size == -1) {
-    log_event(program_name, ERROR, "ftell failed.", log_to_file);
-    fclose(file);
-    return NULL;
+    fprintf(stderr, "Failed to get file position indicator: %s\n",
+            strerror(errno));
+    if (fclose(file) == -1) {
+      fprintf(stderr, "Failed to close file %s:%s\n", file_path,
+              strerror(errno));
+      return NULL;
+    }
   }
+
   *file_size = (size_t)size;
 
   rewind(file);
 
-  unsigned char *buffer;
-  buffer = (unsigned char *)malloc(*file_size);
+  unsigned char *buffer = (unsigned char *)malloc(*file_size);
   if (!buffer) {
-    log_event(program_name, ERROR, "Failed to allocate memory for file buffer.",
-              log_to_file);
-    fclose(file);
+    fprintf(stderr, "Failed to allocate memory for file buffer: %s\n",
+            strerror(errno));
+
+    if (fclose(file) == -1) {
+      fprintf(stderr, "Failed to close file %s:%s\n", file_path,
+              strerror(errno));
+    }
     return NULL;
   }
+
   const size_t bytes_read = fread(buffer, 1, *file_size, file);
+
   if (bytes_read != *file_size) {
-    log_event(program_name, ERROR, "Error reading file into buffer.",
-              log_to_file);
+    log_event(ERROR, "Error reading file into buffer.", log_to_file);
     free(buffer);
-    fclose(file);
+
+    if (fclose(file) == -1) {
+      fprintf(stderr, "Failed to close file %s:%s\n", file_path,
+              strerror(errno));
+    }
+
     return NULL;
   }
-  fclose(file);
+
+  if (fclose(file) == -1) {
+    fprintf(stderr, "Failed to close file %s:%s\n", file_path, strerror(errno));
+    return NULL;
+  }
+
   return buffer;
 }
 
-int prepend_program_data_path(const char *program_name, char **path_buffer,
-                              const char *original_path) {
+int prepend_program_data_path(char **path_buffer, const char *original_path) {
   const char *home = getenv("HOME");
   if (!home) {
-    log_event(program_name, ERROR,
-              "Failed to get value of HOME environment variable.", log_to_file);
-    return 0;
-  }
-  if (!*path_buffer) {
-    log_event(program_name, ERROR,
-              "NULL pointer was passed to prepend_program_data_path.",
+    log_event(ERROR, "Failed to get value of HOME environment variable.",
               log_to_file);
     return 0;
   }
+
+  if (!*path_buffer) {
+    log_event(ERROR, "NULL pointer was passed to prepend_program_data_path.",
+              log_to_file);
+    return 0;
+  }
+
   snprintf(*path_buffer, PATH_MAX, "%s/.local/share/%s/%s", home, program_name,
            original_path);
+
   return 1;
 }
 
-int construct_log_path(const char *program_name, char **path_buffer) {
+int construct_log_path(char **path_buffer) {
   const char *home = getenv("HOME");
   if (!home) {
-    log_event(program_name, ERROR,
-              "Failed to get value of HOME environment variable.", log_to_file);
+    log_event(ERROR, "Failed to get value of HOME environment variable.",
+              log_to_file);
     return 0;
   }
+
   if (!*path_buffer) {
-    log_event(program_name, ERROR,
-              "NULL pointer was passed to construct_log_path.", log_to_file);
+    log_event(ERROR, "NULL pointer was passed to construct_log_path.",
+              log_to_file);
     return 0;
   }
+
   snprintf(*path_buffer, PATH_MAX, "%s/.local/state/%s", home, program_name);
+
   return 1;
 }
 
-void log_event(const char *program_name, int log_level, const char *msg,
-               int log_to_file) {
+const char *get_log_level_msg(const int log_level) {
+  switch (log_level) {
+  case DEBUG:
+    return "DEBUG";
+  case INFO:
+    return "INFO";
+  case WARN:
+    return "WARN";
+  case ERROR:
+    return "ERROR";
+  case FATAL:
+    return "FATAL";
+  default:
+    fprintf(stderr, "Invalid log level supplied.\n");
+    return NULL;
+  }
+}
+
+void write_to_log_file(const char *formatted_msg, struct tm *tm) {
+  char log_filename[NAME_MAX];
+
+  snprintf(log_filename, NAME_MAX, "log_%d%02d%02d.txt", tm->tm_year + 1900,
+           tm->tm_mon + 1, tm->tm_mday);
+
+  char *path_buffer = malloc(PATH_MAX);
+  if (!path_buffer) {
+    fprintf(stderr, "Failed to allocate memory for path_buffer: %s\n",
+            strerror(errno));
+    return;
+  }
+
+  if (!construct_log_path(&path_buffer)) {
+    free(path_buffer);
+    return;
+  }
+
+  char *log_path = malloc(PATH_MAX);
+  if (!log_path) {
+    fprintf(stderr, "Failed to allocate memory for log_path: %s\n",
+            strerror(errno));
+    free(path_buffer);
+    return;
+  }
+
+  if (!file_exists(path_buffer)) {
+    if (mkdir(path_buffer, 0700) == -1) {
+      fprintf(stderr, "Failed to make log directory: %s\n", strerror(errno));
+      free(log_path);
+      free(path_buffer);
+      return;
+    }
+  }
+
+  snprintf(log_path, PATH_MAX, "%s/%s", path_buffer, log_filename);
+  FILE *file = fopen(log_path, "a+");
+  if (!file) {
+    fprintf(stderr, "Failed to open file %s:%s\n", log_path, strerror(errno));
+    free(path_buffer);
+    return;
+  }
+
+  fprintf(file, "%s", formatted_msg);
+
+  if (fclose(file) == -1) {
+    fprintf(stderr, "Failed to close file %s:%s\n", log_path, strerror(errno));
+  }
+
+  free(path_buffer);
+
+  free(log_path);
+}
+
+void log_event(int log_level, const char *msg, int log_to_file) {
+  if (!program_name) {
+    fprintf(stderr, "NULL program name");
+    return;
+  }
+
   if (!msg) {
     fprintf(stderr, "NULL log message.\n");
     return;
   }
+
   if (msg[0] == '\0') {
     fprintf(stderr, "Empty log message.\n");
     return;
   }
 
-  char *log_level_msg;
-  switch (log_level) {
-  case DEBUG:
-    log_level_msg = "DEBUG";
-    break;
-  case INFO:
-    log_level_msg = "INFO";
-    break;
-  case WARN:
-    log_level_msg = "WARN";
-    break;
-  case ERROR:
-    log_level_msg = "ERROR";
-    break;
-  case FATAL:
-    log_level_msg = "FATAL";
-    break;
-  default:
-    fprintf(stderr, "Invalid log level supplied.\n");
+  const char *log_level_msg = get_log_level_msg(log_level);
+  if (!log_level_msg) {
     return;
   }
 
   const time_t t = time(NULL);
+  if (t == -1) {
+    fprintf(stderr, "Failed to get time: %s", strerror(errno));
+  }
+
   struct tm tm = *localtime(&t);
+  if (!tm.tm_hour) {
+    fprintf(stderr, "Failed to get time: %s", strerror(errno));
+    return;
+  }
+
   char formatted_msg[LOG_MAX];
   snprintf(formatted_msg, LOG_MAX, "[%d/%02d/%02d %02d:%02d:%02d] %s  %s\n",
            tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900, tm.tm_hour, tm.tm_min,
            tm.tm_sec, log_level_msg, msg);
 
-  if (log_level > 1) {
+  if (log_level > INFO) {
     fprintf(stderr, "%s", formatted_msg);
   } else {
     printf("%s", formatted_msg);
   }
 
   if (log_to_file) {
-    char log_filename[NAME_MAX];
-    snprintf(log_filename, NAME_MAX, "log_%d%02d%02d.txt", tm.tm_year + 1900,
-             tm.tm_mon + 1, tm.tm_mday);
-    char *path_buffer = malloc(PATH_MAX);
-    if (!path_buffer) {
-      fprintf(stderr, "Failed to allocate memory for path_buffer.\n");
-      return;
-    }
-
-    if (!construct_log_path(program_name, &path_buffer)) {
-      free(path_buffer);
-      return;
-    }
-
-    char *log_path = malloc(PATH_MAX);
-    if (!log_path) {
-      fprintf(stderr, "Failed to allocate memory for log_path.\n");
-      free(path_buffer);
-      return;
-    }
-
-    if (!file_exists(path_buffer)) {
-      if (mkdir(path_buffer, 0700) == -1) {
-        fprintf(stderr, "Failed to make log directory.\n");
-        free(path_buffer);
-        return;
-      }
-    }
-
-    snprintf(log_path, PATH_MAX, "%s/%s", path_buffer, log_filename);
-    FILE *file = fopen(log_path, "a+");
-    if (!file) {
-      fprintf(stderr, "Failed to open log file.\n");
-      free(path_buffer);
-      return;
-    }
-
-    fprintf(file, "%s", formatted_msg);
-    fclose(file);
-    free(path_buffer);
+    write_to_log_file(formatted_msg, &tm);
   }
 }
--- libnn.h
+++ libnn.h
@@ -1,6 +1,7 @@
 #ifndef LIBNN_H
 #define LIBNN_H
 #include <linux/limits.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,27 +11,19 @@
 #define NULL_TERMINATOR_LENGTH 1
 #define LOG_MAX 1024
 
-extern int log_to_file;
-
 extern const char *program_name;
-
-extern const char *malloc_err;
-
-extern const char *prepend_err;
+extern int log_to_file;
 
 // File Operations
 int file_exists(const char *filename);
 char *get_file_extension(const char *file_path);
-unsigned char *read_file(const char *program_name, const char *file_path,
-                         size_t *file_size);
+unsigned char *read_file(const char *file_path, size_t *file_size);
 
 // Logging
 enum levels { DEBUG, INFO, WARN, ERROR, FATAL };
-void log_event(const char *program_name, int log_level, const char *msg,
-               int log_to_file);
+void log_event(int log_level, const char *msg, int log_to_file);
 
 // String Operations
-int prepend_program_data_path(const char *program_name, char **path_buffer,
-                              const char *original_path);
+int prepend_program_data_path(char **path_buffer, const char *original_path);
 
 #endif
